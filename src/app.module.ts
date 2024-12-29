302dc09c92823b01e1a54097782c0263a07791ae
shoharto <bayes@graduate.utm.my>
2024-12-30 01:26:29 +0800

    Integrate configuration management and TypeORM setup in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import databaseConfig from './config/database.config';
+import envConfig from './config/env.config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, envConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
+        const config = configService.get<TypeOrmModuleOptions>('database');
+        if (!config) {
+          throw new Error('Database configuration not found');
+        }
+        return config;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
